062d3521daebd77204b2a41b93e3c3689c90c1aa
Ashraf <ashrafmohamud@gmail.com>
2020-06-14 18:11:22 +0100

    added some dictionary work and a bizz_buzz of sorts...

--- bizz_buzz.py
+++ bizz_buzz.py
@@ -17,9 +17,11 @@
 
 def bizz_buzz():
     do_you_wanna_play = input('Do you want to play? y/n')
-    while do_you_wanna_play.lower() != 'y':
+    while do_you_wanna_play.lower() == True:
+        if do_you_wann
         return
     else:
+        print('goodbye!')
         user_input = int(input('what number would you like to go up to?'))
         for i in range(1, (user_input + 1)):
             if i % 15 == 0:
@@ -32,3 +34,19 @@ def bizz_buzz():
                 print(i)
 
 bizz_buzz()
+
+# pen_pineapple_pen = 100
+# while True:
+#     number = int(input('Choose a number and ill tell you if its a multiple of 3 or 5 or neither? '))
+#     for number in range(1, number + 1):
+#         if number == pen_pineapple_pen:
+#             print("You beat the game!")
+#             break
+#         elif number % 15 == 0:
+#             print("FIZZBUZZ")
+#         elif number % 5 == 0:
+#             print("BUZZ")
+#         elif number % 3 == 0:
+#             print("FIZZ")
+#         else:
+#             print(number)
---
+++ dictionary_story.py
@@ -0,0 +1,17 @@
+story1 = {'start': 'kidnap', 'middle': 'fight scenes', 'end':'reunited'}
+
+print(story1)
+
+print(type(story1))
+
+print(story1.keys())
+
+print(story1.values())
+
+print(story1['start'])
+print(story1['middle'])
+print(story1['end'])
+
+story1['Title'] = 'Taken'
+
+print(story1)
