025c321a5ce9ba7abd1864deeeaf21d806ea4335
Ashraf <ashrafmohamud@gmail.com>
2020-06-14 14:42:50 +0100

    added 2nd exercise

---
+++ exercise_2.py
@@ -0,0 +1,26 @@
+# lists exercise
+
+example_list = ['football', 'toy car', 'iPhone', 'PS4', 'headset']
+
+print(example_list)
+
+print(type(example_list))
+
+print(example_list[0])
+
+print(example_list[1])
+
+print(example_list[-1])
+
+example_list[0] = 'rugby ball'
+
+example_list[1] = 'robot dog'
+
+print(example_list)
+example_list.append('toy gun')
+
+example_list.remove('headset')
+
+example_list.pop()
+
+print(example_list)
